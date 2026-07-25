9284a576a4ce82c4909edd440d358443ede64c05
albinmaniac <www.albinmania@gmail.com>
2026-07-25 07:51:39 +0530

    newly added

--- notices/models.py
+++ notices/models.py
@@ -136,10 +136,6 @@ class Notice(models.Model):
                 }
             )
 
-    def save(self, *args, **kwargs):
-
-        self.full_clean()
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
