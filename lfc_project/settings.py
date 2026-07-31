b566d70804c28535d7ac71125211885b5ac84111
albinmaniac <www.albinmania@gmail.com>
2026-07-31 12:21:55 +0530

    added

--- lfc_project/settings.py
+++ lfc_project/settings.py
@@ -302,10 +302,9 @@ CLOUDINARY_STORAGE = {
 # Celery Configuration
 # ===========================
 
-CELERY_BROKER_URL = os.getenv(
-    "CELERY_BROKER_URL",
-    "redis://127.0.0.1:6379/0",
-)
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
+
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 
 CELERY_ACCEPT_CONTENT = ["json"]
 
